6a6ab92836b95da4e7390a04816e253655fc7421
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-08-24 22:28:13 -0300

    feat: improve accessibility

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,8 @@ const overlayEffect = document.querySelector('.overlay-effect')
 openMenuButton.addEventListener('click', () => {
     overlayMenu.classList.remove('hidden')
     overlayEffect.classList.remove('hidden')
+
+    openMenuButton.setAttribute('aria-expanded', 'true')
 })
 
 
@@ -16,4 +18,5 @@ closeMenuButton.addEventListener('click', () => {
     overlayMenu.classList.add('hidden')
     overlayEffect.classList.add('hidden')
 
+    openMenuButton.setAttribute('aria-expanded', 'false')
 })
